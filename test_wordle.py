4fea618a62d507f16efad1196c5b8dba34a104cb
David Stinnette <dastinnette@gmail.com>
2024-01-29 20:21:22 -0500

    Imported functions to test file

--- test_wordle.py
+++ test_wordle.py
@@ -1,6 +1,9 @@
+from wordle import load_dictionary
+from wordle import is_valid_guess
+from wordle import evaluate_guess
+
 def func(x):
     return x + 1
-
 
 def test_answer():
     assert func(3) == 5
