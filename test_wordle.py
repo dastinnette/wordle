4fb799edc62a1842b361aebf3acc346536bdb511
David Stinnette <dastinnette@gmail.com>
2024-01-30 21:58:54 -0500

    Tested is_valid_guess

--- test_wordle.py
+++ test_wordle.py
@@ -2,8 +2,8 @@ from wordle import load_dictionary
 from wordle import is_valid_guess
 from wordle import evaluate_guess
 
-def func(x):
+def is_valid_guess(x):
     return x + 1
 
-def test_answer():
-    assert func(3) == 5
+def test_valid_guess():
+    assert is_valid_guess(3) == 5
